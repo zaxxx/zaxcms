d278dd7ca0263dd0c1e611854464c24212e505e0
zaxxx <zax.drummer@gmail.com>
2014-07-23 09:59:48 +0200

    WTF indents...

--- zax/Application/UI/Control.php
+++ zax/Application/UI/Control.php
@@ -2,8 +2,8 @@
 
 namespace Zax\Application\UI;
 use Nette,
-    Nette\Utils\Strings,
-    Zax,
+	Nette\Utils\Strings,
+	Zax,
 	Kdyby;
 
 /**
@@ -25,16 +25,16 @@ abstract class Control extends Nette\Application\UI\Control {
 	use Zax\Traits\TTranslatable;
 
 	/** @persistent */
-    public $view = 'Default';
+	public $view = 'Default';
 
 	/** @var string */
 	protected $locale;
 
 	/** @var bool */
-    protected $ajaxEnabled = FALSE;
+	protected $ajaxEnabled = FALSE;
 
 	/** @var bool */
-    protected $autoAjax = FALSE;
+	protected $autoAjax = FALSE;
 
 	/** @var array */
 	protected $ajaxDisabledFor = [];
@@ -50,8 +50,8 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return void
 	 */
 	public function flashMessage($message, $type='info') {
-        $this->presenter->flashMessage($message, $type);
-    }
+		$this->presenter->flashMessage($message, $type);
+	}
 
 	/**
 	 * Enables AJAX for this component AND all sub-components
@@ -61,11 +61,11 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return $this
 	 */
 	public function enableAjax($autoAjax = TRUE, $exclude = []) {
-        $this->ajaxEnabled = TRUE;
-        $this->autoAjax = $autoAjax;
+		$this->ajaxEnabled = TRUE;
+		$this->autoAjax = $autoAjax;
 		$this->disableAjaxFor($exclude);
-        return $this;
-    }
+		return $this;
+	}
 
 	/**
 	 * Forces AJAX off on specified subcomponents (has higher priority than enableAjax())
@@ -89,13 +89,13 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return bool
 	 */
 	public function hasPersistentProperty($property) {
-        $ref = $this->getReflection();
-        if($ref->hasProperty($property)) {
-            $refp = $ref->getProperty($property);
-            return $refp->isPublic() && $refp->hasAnnotation('persistent');
-        }
-        return FALSE;
-    }
+		$ref = $this->getReflection();
+		if($ref->hasProperty($property)) {
+			$refp = $ref->getProperty($property);
+			return $refp->isPublic() && $refp->hasAnnotation('persistent');
+		}
+		return FALSE;
+	}
 
 	/**
 	 * Forward using $presenter->forward()
@@ -104,16 +104,16 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @param array $args
 	 */
 	public function presenterForward($destination, $args = []) {
-        $name = $this->getUniqueId();
-        if($destination != 'this') {
-            $destination =  "$name-$destination";
-        }
-        $params = [];
-        foreach($args as $key => $val) {
-            $params["$name-$key"] = $val;
-        }
-        $this->presenter->forward($destination, $params);
-    }
+		$name = $this->getUniqueId();
+		if($destination != 'this') {
+			$destination =  "$name-$destination";
+		}
+		$params = [];
+		foreach($args as $key => $val) {
+			$params["$name-$key"] = $val;
+		}
+		$this->presenter->forward($destination, $params);
+	}
 
 	/**
 	 * Custom hacky forward, is 'good enough' and additionaly sends anchor in payload
@@ -123,47 +123,47 @@ abstract class Control extends Nette\Application\UI\Control {
 	 */
 	public function forward($destination, $args = []) {
 
-        // Remove '!' from destination
-        $destination = str_replace('!', '', $destination);
+		// Remove '!' from destination
+		$destination = str_replace('!', '', $destination);
 
-        // Remove anchor from destination and insert anchor to payload
-        $anchor = strpos($destination, '#');
-        if(is_int($anchor)) {
-            list($destination, $anchor) = explode('#', $destination);
-            if($this->ajaxEnabled && $this->presenter->isAjax()) {
-                $this->presenter->payload->anchor = $anchor;
-            }
-        }
+		// Remove anchor from destination and insert anchor to payload
+		$anchor = strpos($destination, '#');
+		if(is_int($anchor)) {
+			list($destination, $anchor) = explode('#', $destination);
+			if($this->ajaxEnabled && $this->presenter->isAjax()) {
+				$this->presenter->payload->anchor = $anchor;
+			}
+		}
 
-        // Process arguments
-        $params = [];
-        foreach($args as $key =>$param) {
-            $control = $this;
-            $property = $key;
+		// Process arguments
+		$params = [];
+		foreach($args as $key =>$param) {
+			$control = $this;
+			$property = $key;
 
-            // Get subcomponent from name
-            if(strpos($key, self::NAME_SEPARATOR) > 0) {
-                $names = explode(self::NAME_SEPARATOR, $key);
-                $property = array_pop($names);
-                $control = $this->getComponent(implode(self::NAME_SEPARATOR, $names));
-            }
+			// Get subcomponent from name
+			if(strpos($key, self::NAME_SEPARATOR) > 0) {
+				$names = explode(self::NAME_SEPARATOR, $key);
+				$property = array_pop($names);
+				$control = $this->getComponent(implode(self::NAME_SEPARATOR, $names));
+			}
 
-            if($property == 'view') {
-                $control->setView($param);
-            }else if($control->hasPersistentProperty($property)) {
-                $control->$property = $param;
-            } else {
-                $params[$key] = $param;
-            }
-        }
-        $this->params = $params;
+			if($property == 'view') {
+				$control->setView($param);
+			}else if($control->hasPersistentProperty($property)) {
+				$control->$property = $param;
+			} else {
+				$params[$key] = $param;
+			}
+		}
+		$this->params = $params;
 
-        // No signal
-        if($destination == 'this')
-            return;
-        
-        $this->signalReceived($destination);
-    }
+		// No signal
+		if($destination == 'this')
+			return;
+
+		$this->signalReceived($destination);
+	}
 
 	/**
 	 * If AJAX forward, else redirect
@@ -174,43 +174,43 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @param bool  $presenterForward Prefer $presenter->forward() over $this->forward()
 	 */
 	final public function go($destination, $args = [], $snippets = [], $presenterForward = FALSE) {
-        if($this->ajaxEnabled && $this->presenter->isAjax()) {
-            foreach($snippets as $snippet) {
-                $this->redrawControl($snippet);
-            }
+		if($this->ajaxEnabled && $this->presenter->isAjax()) {
+			foreach($snippets as $snippet) {
+				$this->redrawControl($snippet);
+			}
 
-	        if($presenterForward) {
-		        $this->presenterForward($destination, $args);
-	        } else {
-	            $this->forward($destination, $args);
-	        }
-        } else {
-            $this->redirect($destination, $args);
-        }
-    }
+			if($presenterForward) {
+				$this->presenterForward($destination, $args);
+			} else {
+				$this->forward($destination, $args);
+			}
+		} else {
+			$this->redirect($destination, $args);
+		}
+	}
 
 	/**
 	 * @param $view
 	 * @return string
 	 */
 	static public function formatViewMethod($view) {
-        return 'view' . Strings::firstUpper($view);
-    }
+		return 'view' . Strings::firstUpper($view);
+	}
 
 	/**
 	 * @param $render
 	 * @return string
 	 */
 	static public function formatBeforeRenderMethod($render) {
-        return 'beforeRender' . Strings::firstUpper($render);
-    }
+		return 'beforeRender' . Strings::firstUpper($render);
+	}
 
 	/**
 	 * @param $view
 	 */
 	public function setView($view) {
-        $this->view = Strings::firstUpper($view);
-    }
+		$this->view = Strings::firstUpper($view);
+	}
 
 	/**
 	 * Throws exception if view name contains anything else than alphanumeric characters.
@@ -219,23 +219,23 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @throws Nette\Application\UI\BadSignalException
 	 */
 	protected function checkView($view) {
-        if(!preg_match('/^([a-zA-Z0-9]+)$/', $view)) {
-            throw new Nette\Application\UI\BadSignalException("Signal or view name must be alphanumeric.");
-        }
-    }
+		if(!preg_match('/^([a-zA-Z0-9]+)$/', $view)) {
+			throw new Nette\Application\UI\BadSignalException("Signal or view name must be alphanumeric.");
+		}
+	}
 
 	/**
 	 * Automatic snippet invalidation
 	 */
 	public function attached($presenter) {
-        parent::attached($presenter);
+		parent::attached($presenter);
 		if($this->translator === NULL) {
 			$this->translator = $presenter->translator;
 		}
-        if($this->autoAjax && $this->ajaxEnabled && $presenter->isAjax()) {
-            $this->redrawControl();
-        }
-    }
+		if($this->autoAjax && $this->ajaxEnabled && $presenter->isAjax()) {
+			$this->redrawControl();
+		}
+	}
 
 	/**
 	 * Descendants can override this method to customize templates hierarchy
@@ -245,8 +245,8 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return string
 	 */
 	public function getTemplatePath($view, $render = '') {
-        return dirname($this->reflection->fileName) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $view . (strlen($render) > 0 ? '.' . $render : '') . '.latte';
-    }
+		return dirname($this->reflection->fileName) . DIRECTORY_SEPARATOR . 'templates' . DIRECTORY_SEPARATOR . $view . (strlen($render) > 0 ? '.' . $render : '') . '.latte';
+	}
 
 	/**
 	 * Template helpers factory
@@ -254,10 +254,10 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return Zax\Latte\Helpers
 	 */
 	protected function createTemplateHelpers() {
-        $helpers = new Zax\Latte\Helpers;
-	    $helpers->setTranslator($this->getTranslator());
-	    return $helpers;
-    }
+		$helpers = new Zax\Latte\Helpers;
+		$helpers->setTranslator($this->getTranslator());
+		return $helpers;
+	}
 
 	/* TODO: some stuff for Texy ;-)
 	 public function netteLinksHandler($invocation, $phrase, $content, $modifier, $link) {
@@ -294,19 +294,19 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return Nette\Application\UI\ITemplate
 	 */
 	public function createTemplate() {
-        $this->checkView($this->view);
-        $template = parent::createTemplate();
-	    $template->setTranslator($this->getTranslator());
+		$this->checkView($this->view);
+		$template = parent::createTemplate();
+		$template->setTranslator($this->getTranslator());
 		$template->currentLocale = $this->getLocale();
 		$template->availableLocales = $this->getAvailableLocales();
-        $helpers = $this->createTemplateHelpers();
-        $template->getLatte()->addFilter(NULL, [$helpers, 'loader']);
-	    $texy = $this->createTexy();
-	    $template->getLatte()->addFilter('texy', [$texy, 'process']);
-        $template->ajaxEnabled = $this->ajaxEnabled;
-        $template->view = $this->view;
-        return $template;
-    }
+		$helpers = $this->createTemplateHelpers();
+		$template->getLatte()->addFilter(NULL, [$helpers, 'loader']);
+		$texy = $this->createTexy();
+		$template->getLatte()->addFilter('texy', [$texy, 'process']);
+		$template->ajaxEnabled = $this->ajaxEnabled;
+		$template->view = $this->view;
+		return $template;
+	}
 
 	protected function createComponent($name) {
 		$control = parent::createComponent($name);
@@ -319,7 +319,7 @@ abstract class Control extends Nette\Application\UI\Control {
 	/**
 	 * replacement for render(), descendants should override this method
 	 */
-    protected function beforeRender() {}
+	protected function beforeRender() {}
 
 	/**
 	 * Life cycle
@@ -328,21 +328,21 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @throws \Nette\Application\UI\BadSignalException
 	 */
 	final public function run($render = '') {
-        $template = $this->getTemplate();
-        $template->setFile($this->getTemplatePath($this->view, $render));
-        
-        if(!$this->tryCall($this->formatViewMethod($this->view), $this->params)) {
-            $class = get_class($this);
-            throw new Nette\Application\UI\BadSignalException("There is no handler for view '$this->view' in class $class.");
-        }
-        
-        if(!$this->tryCall($this->formatBeforeRenderMethod($render), array())) {
-            $class = get_class($this);
-            throw new Nette\Application\UI\BadSignalException("There is no 'beforeRender$render' method in class $class.");
-        }
-        
-        $template->render();
-    }
+		$template = $this->getTemplate();
+		$template->setFile($this->getTemplatePath($this->view, $render));
+
+		if(!$this->tryCall($this->formatViewMethod($this->view), $this->params)) {
+			$class = get_class($this);
+			throw new Nette\Application\UI\BadSignalException("There is no handler for view '$this->view' in class $class.");
+		}
+
+		if(!$this->tryCall($this->formatBeforeRenderMethod($render), array())) {
+			$class = get_class($this);
+			throw new Nette\Application\UI\BadSignalException("There is no 'beforeRender$render' method in class $class.");
+		}
+
+		$template->render();
+	}
 
 	/**
 	 * @param SnippetGenerators\ISnippetGenerator $snippetGenerator
@@ -378,10 +378,10 @@ abstract class Control extends Nette\Application\UI\Control {
 	 * @return mixed|void
 	 */
 	public function __call($func, $args = []) {
-        if (Strings::startsWith($func, 'render')) {
-            return $this->run(Strings::substring($func, 6));
-        }
-        return parent::__call($func, $args);
-    }
-    
+		if (Strings::startsWith($func, 'render')) {
+			return $this->run(Strings::substring($func, 6));
+		}
+		return parent::__call($func, $args);
+	}
+
 }
