2de21d725c4a4dc225a784a23cd3064795f7c3b2
zaxxx <zax.drummer@gmail.com>
2014-08-31 11:46:14 +0200

    Added "select icon" input (got trolled by .gitignore)

---
+++ zax/Forms/Controls/Icon/Icon.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Zax\Forms\Controls;
+use Nette,
+	Nette\Forms\Helpers,
+	Nette\Forms\Form,
+	Nette\Utils\Html;
+
+class IconInput extends BaseControl {
+
+	/** @persistent */
+	public $selectedValue;
+
+	public function getValue() {
+		return $this->selectedValue;
+	}
+
+	public function setValue($value) {
+		$this->selectedValue = $value;
+		return $this;
+	}
+
+	public function beforeRender() {
+		$t = $this->getTemplate();
+		$t->value = $this->getValue();
+		$t->htmlName = $this->getHtmlName();
+		$t->htmlId = $this->getHtmlId();
+		$t->rich = $this->rich;
+	}
+
+	public function handleSelectIcon($icon) {
+		$this->setValue($icon);
+	}
+
+	public function loadState(array $params) {
+		parent::loadState($params);
+
+		if(isset($params['selectedValue'])) {
+			$this->setValue($params['selectedValue']);
+		}
+	}
+
+}
