a3104dec7af1e65b90fb6fde586c0cf55ec792a1
zaxxx <zax.drummer@gmail.com>
2014-11-04 03:05:49 +0100

    Added BaseArticle

--- app/model/CMS/Article/Entity.php
+++ app/model/CMS/Article/Entity.php
@@ -13,89 +13,9 @@ use Zax,
 
 /**
  * @ORM\Entity
- *
- * @property-read int $id
  */
-class Article extends BaseEntity {
+class Article extends BaseArticle {
 
-	/**
-	 * @ORM\Id
-	 * @ORM\Column(type="integer")
-	 * @ORM\GeneratedValue
-	 */
-	protected $id;
 
-	/**
-	 * @ORM\ManyToOne(targetEntity="Category")
-	 * @ORM\JoinColumn(name="category_id", referencedColumnName="id")
-	 */
-	protected $category;
-
-	/**
-	 * @Gedmo\Translatable
-	 * @ORM\Column(type="string", length=255)
-	 */
-	protected $title;
-
-	/**
-	 * @Gedmo\Translatable
-	 * @ORM\Column(type="text", nullable=TRUE)
-	 */
-	protected $perex;
-
-	/**
-	 * @Gedmo\Translatable
-	 * @ORM\Column(type="text", nullable=TRUE)
-	 */
-	protected $content;
-
-	/**
-	 * @Gedmo\Slug(fields={"title"})
-	 * @ORM\Column(name="slug", type="string", length=255, unique=TRUE)
-	 */
-	protected $slug;
-
-	/**
-	 * @Gedmo\Timestampable(on="create")
-	 * @ORM\Column(type="datetime")
-	 */
-	protected $createdAt;
-
-	/**
-	 * @Gedmo\Timestampable(on="update")
-	 * @ORM\Column(type="datetime")
-	 */
-	protected $updatedAt;
-
-	/**
-	 * @ORM\ManyToOne(targetEntity="User")
-	 * @ORM\JoinColumn(name="author_id", referencedColumnName="id")
-	 */
-	protected $author;
-
-	/**
-	 * @ORM\Column(type="boolean")
-	 */
-	protected $isPublic;
-
-	/**
-	 * @ORM\ManyToMany(targetEntity="Tag", inversedBy="articles")
-	 * @ORM\JoinTable(name="tag_article")
-	 * @ORM\OrderBy({"id" = "DESC"})
-	 */
-	protected $tags;
-
-	public function setTags($tags) {
-		$this->tags = $tags;
-	}
-
-	/**
-	 * @Gedmo\Locale
-	 */
-	private $locale;
-
-	public function setTranslatableLocale($locale) {
-		$this->locale = $locale;
-	}
 
 }
