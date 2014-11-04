a025e4dba9bb8a704e00fae9a2ae67720eb4c70e
zaxxx <zax.drummer@gmail.com>
2014-11-04 03:07:00 +0100

    Added BaseForm (intended for project-specific custom inputs)

---
+++ app/BaseForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace ZaxCMS;
+use Zax,
+	Nette,
+	ZaxCMS;
+
+/**
+ * Project-specific base form (for custom input factories etc.)
+ */
+class BaseForm extends Zax\Application\UI\Form {
+
+
+
+}
